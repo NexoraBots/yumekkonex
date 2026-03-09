9f581abc160520ab783a823619aa942d1e48d9cd
NexoraBots <vkboards2627@gmail.com>
2026-03-09 19:31:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,57 +1,56 @@
 class config:
-    
-    #Client
-    API_ID = 0
-    API_HASH = "0"
-    BOT_TOKEN = "0:0-0"
-    BOT_NAME = "Your_Bot_Name"
-    BOT_USERNAME = "Your_Bot_Username"
-    BOT_ID = 0
-    WORKERS = 30
-    MAX_MESSAGE_CACHE_SIZE = 100
-    MAX_CONCURRENT_TRANSMISSIONS = 10
-    
-     
-    #Git
-    GIT_USERNAME = "Your_Git_Username"
-    GIT_URL_WITH_TOKEN = "https://Your_Git_Token@github.com/Your_Git_Username/Yumeko.git" 
+
+  #Client
+  API_ID = 35064036
+  API_HASH = "c9a901b08789b91eae4097ba2dcc086c"
+  BOT_TOKEN = "8708508918:AAGRV-a7SLcxkI3S6ZazM-MWh34bPNQgOwE"
+  BOT_NAME = "Yumeko 🌺"
+  BOT_USERNAME = "YumekkoRoBot"
+  BOT_ID = 8708508918
+  WORKERS = 30
+  MAX_MESSAGE_CACHE_SIZE = 100
+  MAX_CONCURRENT_TRANSMISSIONS = 10
 
 
-    #Info
-    BOT_VERSION = "x"
-    OWNER_ID = 5630057244
-    OWNER_USERNAME = "senpaiii10"
-    SUPPORT_CHAT = -1002069412308
-    SUPPORT_CHAT_USERNAME = "Domihoes"
-    SUPPORT_CHAT_LINK = "https://t.me/Domihoes"
-    LOG_CHANNEL = -1001625036774
-    ERROR_LOG_CHANNEL = -1001625036774
-    DOWNLOAD_LOCATION = "./downloads"
-    COMMAND_PREFIXES = ["/" , "!" , "." , "#" , "$" , "%" , "&" , "?"] 
-    CMD_STARTERS = "/.!&#%$"
-    STATS_IMG_URL = "https://files.catbox.moe/d69tm0.jpg"
-    START_IMG_URL = "https://files.catbox.moe/wybrme.jpg"
-    HELP_IMG_URL = "https://files.catbox.moe/j3tdbi.jpg"
-    ALIVE_IMG_URL = "https://files.catbox.moe/w4t9cj.jpg"
-    
-    
-    #Database
-    MONGODB_URI = "mongodb://localhost:27017" # Use Local Hosted Or Atlas !!
-    DATABASE_NAME = "Frieren"
+  #Git
+  GIT_USERNAME = "NexoraBots"
+  GIT_URL_WITH_TOKEN = "https://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github.com/NexoraBots/Kamisako-akaya.git" 
 
-    #API
-    ARQ_API_KEY = "RLWCED-WZASYO-AWOLTB-ITBWTP-ARQ"
-    ARQ_API_URL = "arq.hamker.dev"
-    CRICKET_API_URL = "https://sugoi-api.vercel.app/cricket"
-    FOOTBALL_API_URL = "https://sugoi-api.vercel.app/football"
-    BINGSEARCH_URL = "https://sugoi-api.vercel.app/search"
-    NEWS_URL = "https://sugoi-api.vercel.app/news?keyword={}"
-    shayri_api_url = "https://hindi-quotes.vercel.app/random"
-    BASE_URL = "https://api.waifu.pics"
-    Movie_Api = "5d3274c3bb08b4276482436c8444abc0"
-    Movie_RAC = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDMyNzRjM2JiMDhiNDI3NjQ4MjQzNmM4NDQ0YWJjMCIsIm5iZiI6MTczMzIyMjgxMy42OTQwMDAyLCJzdWIiOiI2NzRlZTE5ZDJjZTRjZTdkZDYwOTU2YjAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.aXfQw0_CRrKl2iSJd9tFE1TVbWWVYNgysWkUVlwzyRg"
-    Pokedex = "https://sugoi-api.vercel.app/pokemon?name={name_or_id}"
-    OPENAI_KEY = "xx" #Get From Open Ai's Website
-    LYRICS_GENIUS_TOKEN = "x-x" #Get From Genius.com
+  
+  #Info
+  BOT_VERSION = "x"
+  OWNER_ID = 8585494095
+  OWNER_USERNAME = "drekk_so"
+  SUPPORT_CHAT = -1003526121522
+  SUPPORT_CHAT_USERNAME = "nexorabotschat"
+  SUPPORT_CHAT_LINK = "https://t.me/nexorabotschat"
+  LOG_CHANNEL = -1003692127639
+  ERROR_LOG_CHANNEL = -1003692127639
+  DOWNLOAD_LOCATION = "./downloads"
+  COMMAND_PREFIXES = ["/" , "!" , "." , "#" , "$" , "%" , "&" , "?"] 
+  CMD_STARTERS = "/.!&#%$"
+  STATS_IMG_URL = "https://files.catbox.moe/d69tm0.jpg"
+  START_IMG_URL = "https://files.catbox.moe/wybrme.jpg"
+  HELP_IMG_URL = "https://files.catbox.moe/j3tdbi.jpg"
+  ALIVE_IMG_URL = "https://files.catbox.moe/w4t9cj.jpg"
 
+
+  #Database
+  MONGODB_URI = "mongodb+srv://wrik:wri13072005@cluster0.hnwak.mongodb.net/?retryWrites=true&w=majority" # Use Local Hosted Or Atlas !!
+  DATABASE_NAME = "Yumekonex"
+
+  #API
+  ARQ_API_KEY = "RLWCED-WZASYO-AWOLTB-ITBWTP-ARQ"
+  ARQ_API_URL = "arq.hamker.dev"
+  CRICKET_API_URL = "https://sugoi-api.vercel.app/cricket"
+  FOOTBALL_API_URL = "https://sugoi-api.vercel.app/football"
+  BINGSEARCH_URL = "https://sugoi-api.vercel.app/search"
+  NEWS_URL = "https://sugoi-api.vercel.app/news?keyword={}"
+  shayri_api_url = "https://hindi-quotes.vercel.app/random"
+  BASE_URL = "https://api.waifu.pics"
+  Movie_Api = "5d3274c3bb08b4276482436c8444abc0"
+  Movie_RAC = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDMyNzRjM2JiMDhiNDI3NjQ4MjQzNmM4NDQ0YWJjMCIsIm5iZiI6MTczMzIyMjgxMy42OTQwMDAyLCJzdWIiOiI2NzRlZTE5ZDJjZTRjZTdkZDYwOTU2YjAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.aXfQw0_CRrKl2iSJd9tFE1TVbWWVYNgysWkUVlwzyRg"
+  Pokedex = "https://sugoi-api.vercel.app/pokemon?name={name_or_id}"
+  OPENAI_KEY = "xx" #Get From Open Ai's Website
+  LYRICS_GENIUS_TOKEN = "x-x" #Get From Genius.com
 
